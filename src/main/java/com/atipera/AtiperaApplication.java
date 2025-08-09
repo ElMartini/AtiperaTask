cd01e28b846f503b6ffb8cbcafc1e740806330d5
ElMartini <markam888@protonmail.com>
2025-08-09 11:04:47 +0200

    First repo getter
    Added basic endpoint's to collect data about repositories from specific user

--- src/main/java/com/atipera/AtiperaApplication.java
+++ src/main/java/com/atipera/AtiperaApplication.java
@@ -1,13 +1,19 @@
 package com.atipera;
 
+import com.atipera.service.GitHubService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.concurrent.ExecutionException;
 
 @SpringBootApplication
 public class AtiperaApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws URISyntaxException, IOException, InterruptedException, ExecutionException {
 		SpringApplication.run(AtiperaApplication.class, args);
+		GitHubService.run();
 	}
 
 }
