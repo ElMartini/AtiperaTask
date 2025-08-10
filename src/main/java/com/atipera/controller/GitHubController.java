e763a1dd43cc2becdc6a5f2d890bf84cadaee0fb
ElMartini <markam888@protonmail.com>
2025-08-10 13:10:14 +0200

    Added Error handling and basic test
    Added method that correctly show results of wrong input (for example user that not exist) and added basic integration test that check happy path

--- src/main/java/com/atipera/controller/GitHubController.java
+++ src/main/java/com/atipera/controller/GitHubController.java
@@ -1,19 +1,29 @@
 package com.atipera.controller;
 
 import com.atipera.service.GitHubService;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
+
+@Component
 public class GitHubController {
 
-    public static void runController() throws URISyntaxException, IOException, InterruptedException {
+    @EventListener(ApplicationReadyEvent.class)
+    public void runController() throws URISyntaxException, IOException, InterruptedException {
 
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Please enter user login: ");
         String user =scanner.nextLine();
-        GitHubService.showReposAndBranches(user);
+        String encodedUser = URLEncoder.encode(user, StandardCharsets.UTF_8);
+        GitHubService gitHubService = new GitHubService();
+        gitHubService.showReposAndBranches(encodedUser);
     }
 }
